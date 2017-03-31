8373488e06a17d1f6d83e3f424d019ed8afb4cf7
yuezaixz <303178394@qq.com>
2017-03-31 16:01:45 +0800

    clean code

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,3 +1,5 @@
+var util = require('../../utils/util.js')
+
 //index.js
 //获取应用实例
 var app = getApp()
@@ -16,37 +18,8 @@ wx.getStorage({
   } 
 })
 
-function char2buf(str){
-  var out = new ArrayBuffer(str.length);
-  var u16a= new Uint8Array(out);
-  var strs = str.split("");
-  for(var i =0 ; i<strs.length;i++){
-      u16a[i]=strs[i].charCodeAt();
-  }
-  return out;
-}
 
-//This function takes in latitude and longitude of two location and returns the distance between them as the crow flies (in km)
-function calcCrow(lat1, lon1, lat2, lon2) 
-{
-  var R = 6371; // km
-  var dLat = toRad(lat2-lat1);
-  var dLon = toRad(lon2-lon1);
-  var lat1 = toRad(lat1);
-  var lat2 = toRad(lat2);
 
-  var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
-    Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2); 
-  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
-  var d = R * c;
-  return d;
-}
-
-// Converts numeric degrees to radians
-function toRad(Value) 
-{
-    return Value * Math.PI / 180
-}
 
 // var startTime = 0
 
@@ -61,7 +34,7 @@ var startRunning =  function(deviceId, serviceId, characteristicId, value) {
       serviceId: serviceId,
       // 这里的 characteristicId 需要在上面的 getBLEDeviceCharacteristics 接口中获取
       characteristicId: characteristicId,
-      value: char2buf(value),
+      value: util.char2buf(value),
       success: function (res) {
         console.log('writeBLECharacteristicValue success', res.errMsg)
       }
@@ -81,7 +54,7 @@ function getLocation(){
       var latitude = res.latitude
       var longitude = res.longitude
       if (lastLocation) {
-        totalDistance += calcCrow(lastLocation.longitude, lastLocation.latitude, longitude, latitude) * 1000
+        totalDistance += util.calcCrow(lastLocation.longitude, lastLocation.latitude, longitude, latitude) * 1000
         console.log("移动"+totalDistance+"米")
       }
       lastLocation = {
--- utils/util.js
+++ utils/util.js
@@ -16,6 +16,40 @@ function formatNumber(n) {
   return n[1] ? n : '0' + n
 }
 
+function char2buf(str){
+  var out = new ArrayBuffer(str.length);
+  var u16a= new Uint8Array(out);
+  var strs = str.split("");
+  for(var i =0 ; i<strs.length;i++){
+      u16a[i]=strs[i].charCodeAt();
+  }
+  return out;
+}
+
+//This function takes in latitude and longitude of two location and returns the distance between them as the crow flies (in km)
+function calcCrow(lat1, lon1, lat2, lon2) 
+{
+  var R = 6371; // km
+  var dLat = toRad(lat2-lat1);
+  var dLon = toRad(lon2-lon1);
+  var lat1 = toRad(lat1);
+  var lat2 = toRad(lat2);
+
+  var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+    Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2); 
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+  var d = R * c;
+  return d;
+}
+
+// Converts numeric degrees to radians
+function toRad(Value) 
+{
+    return Value * Math.PI / 180
+}
+
 module.exports = {
-  formatTime: formatTime
+  formatTime: formatTime,
+  char2buf: char2buf,
+  calcCrow: calcCrow
 }
